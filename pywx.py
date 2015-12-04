5bc7fc13fcc2d571f507a95b0dccaae3c801cd47
Yuxiang Zhou <yz4009@antony.doc.ic.ac.uk>
2015-12-04 19:06:16 +0000

    Managing messages

--- pywx.py
+++ pywx.py
@@ -1,7 +1,5 @@
 import subprocess
-import cookielib
-import urllib
-import urllib2
+import requests
 import pytz, calendar
 from datetime import datetime
 
@@ -30,60 +28,176 @@ from datetime import datetime
 class WXBot(object):
 
     def __init__(self):
-        # Initialise Browser
-        self.cj = cookielib.CookieJar()
-        self.opener = urllib2.build_opener(
-            urllib2.HTTPRedirectHandler(),
-            urllib2.HTTPHandler(debuglevel=0),
-            urllib2.HTTPSHandler(debuglevel=0),
-            urllib2.HTTPCookieProcessor(self.cj)
-        )
-        self.opener.addheaders = [
-            ('User-agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/45.0.2454.85 Chrome/45.0.2454.85 Safari/537.36')
-        ]
-        self.opener.addheaders = [
-            ('Connection', 'keep-alive')
-        ]
-        self.opener.addheaders = [
-            ('Pragma', 'no-cache')
-        ]
-        self.opener.addheaders = [
-            ('Cache-Control', 'no-cache')
-        ]
-        self.opener.addheaders = [
-            ('Accept-Encoding', 'gzip, deflate, sdch')
-        ]
-        self.opener.addheaders = [
-            ('Accept-Language', 'en-GB,en-US;q=0.8,en;q=0.6,it;q=0.4,zh-CN;q=0.2,zh-TW;q=0.2')
-        ]
-
         # Predefine request strings
         self._hrefLoginFormat = 'https://wx.qq.com/jslogin?appid=wx782c26e4c19acffb&redirect_uri=https%3A%2F%2Fwx.qq.com%2Fcgi-bin%2Fmmwebwx-bin%2Fwebwxnewloginpage&fun=new&lang=en_GB&_={}'
 
         self._hrefQRCodeFormat = 'https://login.weixin.qq.com/qrcode/{}'
 
-        self._herfScanFormat = 'https://login.weixin.qq.com/cgi-bin/mmwebwx-bin/login?loginicon=true&uuid={}&tip=0&r=-1765867164&_={}'
+        self._herfScanFormat = 'https://login.weixin.qq.com/cgi-bin/mmwebwx-bin/login?loginicon=true&uuid={}&tip=0&r=-1832847261&_={}'
+
+        self._herfWXInit = 'https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxinit?r=-1832847261&lang=en_GB&pass_ticket={}'
+
+        self._hrefWXGetContacts = 'https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxgetcontact?lang=en_GB&pass_ticket={}&r={}&skey={}'
+
+        self._hrefSynccheck =  'https://webpush.weixin.qq.com/cgi-bin/mmwebwx-bin/synccheck?r={}&skey={}&sid={}&deviceid={}&synckey={}'
+
+        self._hrefWebwxsync = 'https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxsync?sid={}&skey={}&lang=en_GB&pass_ticket={}'
+
+        # Header Settings
+        self.headers = {
+            'Pragma': 'no-cache',
+            'Origin': 'https://wx.qq.com',
+            'Accept-Language': 'en-GB,en-US;q=0.8,en;q=0.6,it;q=0.4,zh-CN;q=0.2,zh-TW;q=0.2',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/45.0.2454.85 Chrome/45.0.2454.85 Safari/537.36',
+            'Content-Type': 'application/json;charset=UTF-8',
+            'Accept': 'application/json, text/plain, */*',
+            'Cache-Control': 'no-cache',
+            'Referer': 'https://wx.qq.com/?&lang=en_GB'
+        }
 
         # global variables
         self.isLoggedIn = False
         self.uuid = ""
+        self.skey = ""
+        self.wxsid = ""
+        self.wxuin = ""
+        self.deviceID = 'e048677534097806'
+        self.pass_ticket = ""
+        self.contacts = []
+        self.synckey = []
 
-    def request_login(self):
-        response = self.opener.open(self._hrefLoginFormat.format(self._get_unix_timestamp()))
+        # Session Management
+        self.session = requests.Session()
+        self.session.headers.update(self.headers)
 
-        self.uuid = self._find_between(response.readlines()[0],'uuid = \"',"\";")
+    def request_get_contacts(self):
+        response = self.session.get(self._hrefWXGetContacts.format(
+            self.pass_ticket,
+            self._get_unix_timestamp(),
+            self.skey
+        ))
+        data = response.json()
+        if data['BaseResponse']['Ret'] == 0:
+            self.contacts = data['MemberList']
+        else:
+            print 'Failed to get contacts list.'
 
-        print self._hrefQRCodeFormat.format(self.uuid)
+    def request_synccheck(self):
 
-        # wait for scan
-        while(not self.isLoggedIn):
-            response = self.opener.open(
-                self._herfScanFormat.format(
-                    self.uuid, self._get_unix_timestamp()
-                )
+        def format_synckey():
+            return '%7C'.join(
+                ['{}_{}'.format(k['Key'], k['Val']) for k in self.synckey]
             )
 
-            print response.readlines()
+        response = self.session.get(self._hrefSynccheck.format(
+            self._get_unix_timestamp(),
+            self.skey,
+            self.wxsid,
+            self.deviceID,
+            format_synckey()
+        ))
+
+        selector = self._find_between(response.text,'selector:"','"}')
+
+        if selector is '0':
+            # increment key 1000 by 1
+            for k in self.synckey:
+                if k['Key'] == 1000:
+                    k['Val'] += 1
+        else:
+            # Get New Messages
+            data = {
+                "BaseRequest":{
+                    "Uin": self.wxuin,
+                    "Sid": self.wxsid,
+                    "Skey": self.skey,
+                    "DeviceID": self.deviceID
+                },"SyncKey":{
+                    "Count":len(self.synckey),
+                    "List":self.synckey
+                },"rr":-1851102809
+            }
+
+            response = self.session.post(self._hrefWebwxsync.format(
+                self.wxsid,
+                self.skey,
+                self.pass_ticket
+            ), json=data)
+
+            print 'Message Recieved:'
+            print response.json()
+
+
+    def request_login(self):
+        while(not self.isLoggedIn):
+            response = self.session.get(self._hrefLoginFormat.format(self._get_unix_timestamp()))
+
+            self.uuid = self._find_between(response.text,'uuid = \"',"\";")
+
+            print self._hrefQRCodeFormat.format(self.uuid)
+
+            # wait for scan
+            while(not self.isLoggedIn):
+                response = self.session.get(
+                    self._herfScanFormat.format(
+                        self.uuid, self._get_unix_timestamp()
+                    )
+                )
+                line = response.text
+                code = self._find_between(line, 'window.code=', ';')
+                if code == "408":
+                    print "Please Scan QR Code"
+                elif code == '201':
+                    print "Please accept login on your phone"
+                elif code == '200':
+                    print 'Initialising...'
+
+                    urlredirect = self._find_between(
+                        line,
+                        'window.redirect_uri="',
+                        '";'
+                    ) + '&fun=new&version=v2&lang=en_GB'
+
+                    # Request confidential information
+                    response = self.session.get(urlredirect)
+                    retCode = self._find_between(response.text, '<ret>', '</ret>')
+                    self.skey = self._find_between(response.text, '<skey>', '</skey>')
+                    self.wxsid = self._find_between(response.text, '<wxsid>', '</wxsid>')
+                    self.wxuin = self._find_between(response.text, '<wxuin>', '</wxuin>')
+                    self.pass_ticket = self._find_between(response.text, '<pass_ticket>', '</pass_ticket>')
+
+                    if not retCode == '0':
+                        break
+
+                    print 'Confidential Info Retrieved:'
+                    print self.skey, self.wxsid, self.wxuin, self.pass_ticket
+
+                    data = {}
+                    data['BaseRequest'] = {}
+                    data['BaseRequest']['DeviceID'] = self.deviceID
+                    data['BaseRequest']['Sid'] = self.wxsid
+                    data['BaseRequest']['Skey'] = self.skey
+                    data['BaseRequest']['Uin'] = self.wxuin
+
+                    response = self.session.post(
+                        self._herfWXInit.format(self.pass_ticket),
+                        json=data
+                    )
+                    print response.text
+                    self.init_info = response.json()
+
+                    if not self.init_info['BaseResponse']['Ret'] == 0:
+                        print 'Failed to init with server. Retrying...'
+                        break
+
+                    self.synckey = data['SyncKey']['List']
+
+                    self.isLoggedIn = True
+                    print 'Logged In as ' + self.init_info['User']['NickName']
+                else:
+                    print line
+                    print 'Login Failed, Retrying...'
+                    break
 
     def run(self):
         self.request_login()
